c1de48e3549b8b8267ccf099867f08c0b73d6126
Abisuwa Oluwadunsin <ayodejiabisuwa23@gmail.com>
2024-10-28 16:53:18 +0100

    gottne my near balcne

--- src/components/Profile/Stat.tsx
+++ src/components/Profile/Stat.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { NearContext } from "@/wallets/near";
+import React, { useEffect, useState } from "react";
 import { FaFlag, FaBolt, FaUser, FaHandHolding } from "react-icons/fa";
 
 const DashboardCards = () => {
+  const { wallet, signedAccountId } = React.useContext(NearContext);
+  const [nearBalance, setNearBalance] = useState(0);
+  const getBalance = async () => {
+    if (wallet && signedAccountId) {
+      console.log(wallet);
+      try {
+        const balance = await wallet?.getBalance(signedAccountId);
+        setNearBalance(balance);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  useEffect(() => {
+    getBalance();
+  }, []);
   return (
     <div className="grid grid-cols-3 gap-4 p-4  text-white mx-auto rounded-lg">
       {/* Campaigns Card */}
@@ -35,9 +53,11 @@ const DashboardCards = () => {
       <div className="flex flex-col items-center bg-gray-900 p-4 rounded-lg">
         <div className="flex items-center space-x-2">
           <FaHandHolding className="text-blue-500" />
-          <span className="text-xl font-semibold">NEAR</span>
+          <span className="text-xl font-semibold">
+            {nearBalance.toPrecision(3)}
+          </span>
         </div>
-        <p className="mt-2 text-gray-400 text-sm">0</p>
+        <p className="mt-2 text-gray-400 text-sm">NEAR</p>
       </div>
     </div>
   );
